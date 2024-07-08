a61a9bb6493c75baa92fd4a6e7096424030245f2
Rakshit Sain <rakshitsain895@gmail.com>
2024-07-08 19:49:58 -0400

    chapter14

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -18,8 +18,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+ 
   return (
     <form action={updateInvoiceWithId}>
       <input type="hidden" name="id" value={invoice.id} />
